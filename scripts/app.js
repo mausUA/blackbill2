8c7b0ccb6579e7b1867105ff7865522eaafe4728
maus <dd>
2016-03-11 21:52:22 +0200

    First run

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,26 @@
 (function () {
-var app = angularModule('bbill',[]);
+    "use strict";
 
-    app.controller('picCtrl', function () {
- this.gallery = gal_content;
-});
-var gal_content = [
-    {type: 'building',
-    pic: 'pics/ch1.jpg',
-    name: 'Reactor',
-    desc: 'Crazy construction'},
-    {type: 'landscape',
-    pic: 'pics/ch2.jpg',
-    name: 'Overview',
-    desc: 'View on reactor'},
-    {type: 'interior',
-        pic: 'ch5.jpg',
-        name: 'Controller',
-        desc: 'Soviet remote control :)'
-            }
-]
+    var app = angular.module('MyApp', []);
 
-});
+    app.controller('picControl', function () {
+        this.gall =  [
+            {type: 'building',
+                pic: 'pics/ch1.jpg',
+                name: 'Reactor',
+                desc: 'Crazy construction'},
+            {type: 'landscape',
+                pic: 'pics/ch2.jpg',
+                name: 'Overview',
+                desc: 'View on reactor'},
+            {type: 'interior',
+                pic: 'pics/ch5.jpg',
+                name: 'Controller',
+                desc: 'Soviet remote control :)'
+            }];
+    });
+})();
+
+
+
+
